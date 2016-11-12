f5274c0064025c3167d897aaa3b05388d6fca894
sukyung <acacia529@gmail.com>
2016-11-12 20:32:31 -0300

    ensayo angular

---
+++ app/hero.js
@@ -0,0 +1,17 @@
+"use strict";
+var Hero = (function () {
+    function Hero() {
+        this.id = 1;
+        this.name = "Windstorm";
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var Title = (function () {
+    function Title() {
+        this.title = "Titulo";
+    }
+    return Title;
+}());
+exports.Title = Title;
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,8 @@
+export class Hero{
+  id: number = 1;
+  name: string = "Windstorm";
+}
+
+export class Title{
+    title: string = "Titulo";
+}
